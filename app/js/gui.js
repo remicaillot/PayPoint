42b9bf2fd6ef6d1a4f01f1d9ae4c21272aecea13
Orbital Blaze <remicaillot5@gmail.com>
2016-01-23 11:25:18 +0100

    keyboard opening methods

--- app/js/gui.js
+++ app/js/gui.js
@@ -20,5 +20,35 @@ jQuery(document).ready(function($) {
 		$(".categorie").css('width', '50px');
 		$(".categorie:active .catContent").css('-webkit-transform', 'scale3d(0.93,0.93,0.93)');
 	});
+	$(".categorie").click(function(event) {
+		if($(this).attr('id') != "sideBarMenu"){
+			$(".categorie").removeClass('activated');
+			$(this).addClass('activated');
+			$("#leftPanel").attr("open", false);
+			$("#leftPanel").css('width', '50px');
+			$(".categorie").css('width', '50px');
+			$(".categorie:active .catContent").css('-webkit-transform', 'scale3d(0.93,0.93,0.93)');
+		}		
+	});
+
+	var currentFocusedElement;
+	$("input[type='text']").focus(function(event) {
+		$("#alphanumericPad").show();
+		$("#numericPad").hide();
+		$("#keyboard").css('bottom', '0');
+		currentFocusedElement = $(this);
+		currentFocusedElement.addClass('focused');
+	});
+	$('html').click(function() {
+		$("#keyboard").css('bottom', '-350px');
+		currentFocusedElement.removeClass('focused');
+	});
+	$("input[type='text']").click(function(event){
+	    event.stopPropagation();
+	});
+	$("#keyboard").click(function(event){
+	    event.stopPropagation();
+		currentFocusedElement.addClass('focused');
+	});
 });
 	
