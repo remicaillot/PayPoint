6071ac0eeb44530980d47b88ebcb7ff78d42ed1e
Luxgnite <ewan.dubois@live.fr>
2016-02-01 20:37:07 +0100

    Début script
    Ai  inscrit une fonction jQuery pour récupérer la valeur de la touche
    pressé et la mettre dans la value de l'input ciblé

--- app/libs/libKeyboardCtrl.js
+++ app/libs/libKeyboardCtrl.js
@@ -1,8 +1,16 @@
-var currentFocusedElement;
-	$("input[type='text']").focus(function(event) {
-		$("#alphanumericPad").show();
-		$("#numericPad").hide();
-		$("#keyboard").css('bottom', '0');
-		currentFocusedElement = $(this);
-		currentFocusedElement.addClass('focused');
-	});
+var currentFocusedInput;
+
+$("input[type='text']").focus(function(event) {
+	$("#alphanumericPad").show();
+	$("#numericPad").hide();
+	$("#keyboard").css('bottom', '0');
+	currentFocusedInput = $(this);
+	currentFocusedInput.addClass('focused');
+});
+
+$(".key").click(function(event) {
+	var valueOfInput = currentFocusedInput.value();
+	console.log(valueOfInput);
+	valueOfInput += $(this).attr('value');
+	currentFocusedInput.value(valueOfInput);
+});
