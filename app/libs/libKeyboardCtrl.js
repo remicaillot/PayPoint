ad1c17798073c04aa34caf714da38d139169578e
AirCraft <remicaillot5@gmail.com>
2016-02-01 20:40:03 +0100

    template

--- app/libs/libKeyboardCtrl.js
+++ app/libs/libKeyboardCtrl.js
@@ -1,8 +1,9 @@
-var currentFocusedElement;
-	$("input[type='text']").focus(function(event) {
-		$("#alphanumericPad").show();
-		$("#numericPad").hide();
-		$("#keyboard").css('bottom', '0');
-		currentFocusedElement = $(this);
-		currentFocusedElement.addClass('focused');
-	});
+var currentFocusedElement; // current focused input type text jquery identifiant
+$("input[type='text']").focus(function(event) {
+	$("#alphanumericPad").show();
+	$("#numericPad").hide();
+	$("#keyboard").css('bottom', '0');
+	currentFocusedElement = $(this);
+	console.log(currentFocusedElement);
+	currentFocusedElement.addClass('focused');
+});
