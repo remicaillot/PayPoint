c3767e333d53707f82e1340b739d45769cdd9b9c
AirCraft <remicaillot5@gmail.com>
2016-02-01 20:41:16 +0100

    Merge remote-tracking branch 'origin/keyboard' into keyboard
    # Conflicts:
    #	app/libs/libKeyboardCtrl.js

--- app/libs/libKeyboardCtrl.js
+++ app/libs/libKeyboardCtrl.js
@@ -1,8 +1,8 @@
 var currentFocusedElement;
-	$("input[type='text']").focus(function(event) {
-		$("#alphanumericPad").show();
-		$("#numericPad").hide();
-		$("#keyboard").css('bottom', '0');
-		currentFocusedElement = $(this);
-		currentFocusedElement.addClass('focused');
-	});
+$("input[type='text']").focus(function(event) {
+	$("#alphanumericPad").show();
+	$("#numericPad").hide();
+	$("#keyboard").css('bottom', '0');
+	currentFocusedElement = $(this);
+	currentFocusedElement.addClass('focused');
+});
