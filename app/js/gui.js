741a56f5eff3b232624420c9c31ddb42c6164992
AirCraft <remicaillot5@gmail.com>
2016-04-15 10:14:20 +0200

    navigation complete

--- app/js/gui.js
+++ app/js/gui.js
@@ -1,61 +1,73 @@
 jQuery(document).ready(function($) {
-	$("#sideBarMenu").click(function(e){
-		if($("#leftPanel").attr("open") == "open"){
-			$("#leftPanel").attr("open", false);
-			$("#leftPanel").css('width', '50px');
-			$(".categorie").css('width', '50px');
-
-			$(".categorie:active .catContent").css('-webkit-transform', 'scale3d(0.93,0.93,0.93)');
-		}else{
-			$("#leftPanel").attr("open", true);
-			$(".categorie").css('width', '200px');
-			$("#leftPanel").css('width', '200px');
-			$(".categorie:active .catContent").css('-webkit-transform', 'scale3d(0.89,0.89,0.89)');
-		}
-		
-	});
-	$(".categorie").click(function(event) {
-		if($(this).attr('id') != "sideBarMenu"){
-			$(".categorie").removeClass('activated');
-			$(this).addClass('activated');
-			$("#leftPanel").attr("open", false);
-			$("#leftPanel").css('width', '50px');
-			$(".categorie").css('width', '50px');
-			$(".categorie:active .catContent").css('-webkit-transform', 'scale3d(0.93,0.93,0.93)');
-		}		
-	});
-
-	
-	$('html').click(function() {
-		$("#keyboard").css('bottom', '-350px');
-		$("#leftPanel").attr("open", false);
-		$("#leftPanel").css('width', '50px');
-		$(".categorie").css('width', '50px');
-		$(".categorie:active .catContent").css('-webkit-transform', 'scale3d(0.93,0.93,0.93)');
-		try{
-			currentFocusedInput.removeClass('focused');
-		}catch(e){
-		}
-	});
-	$("input[type='text']").click(function(event){
-	    event.stopPropagation();
-	});
-	$("#leftPanel").click(function(event){
-	    event.stopPropagation();
-	});
-	$("#keyboard").click(function(event){
-	    event.stopPropagation();
-		try{
-			currentFocusedInput.addClass('focused');
-		}catch(e){
-		}
-	});
-	$(".productTile").click(function(event) {
-		console.log($(this).attr("data-objectID") + " is " + $(this).attr("data-objectTYPE"));
-	});
+    $("#sideBarMenu").click(function(e) {
+        if ($("#leftPanel").attr("open") == "open") {
+            $("#leftPanel").attr("open", false);
+            $("#leftPanel").css('width', '50px');
+            $(".categorie").css('width', '50px');
+            $(".categorie:active .catContent").css('-webkit-transform', 'scale3d(0.93,0.93,0.93)');
+        } else {
+            $("#leftPanel").attr("open", true);
+            $(".categorie").css('width', '200px');
+            $("#leftPanel").css('width', '200px');
+            $(".categorie:active .catContent").css('-webkit-transform', 'scale3d(0.89,0.89,0.89)');
+        }
+    });
+    $(".categorie").click(function(event) {
+        if ($(this).attr('id') != "sideBarMenu") {
+            $(".categorie").removeClass('activated');
+            $(this).addClass('activated');
+            $("#leftPanel").attr("open", false);
+            $("#leftPanel").css('width', '50px');
+            $(".categorie").css('width', '50px');
+            $(".categorie:active .catContent").css('-webkit-transform', 'scale3d(0.93,0.93,0.93)');
+        }
+    });
+    $('html').click(function() {
+        $("#keyboard").css('bottom', '-350px');
+        $("#leftPanel").attr("open", false);
+        $("#leftPanel").css('width', '50px');
+        $(".categorie").css('width', '50px');
+        $(".categorie:active .catContent").css('-webkit-transform', 'scale3d(0.93,0.93,0.93)');
+        try {
+            currentFocusedInput.removeClass('focused');
+        } catch (e) {}
+    });
+    $("input[type='text']").click(function(event) {
+        event.stopPropagation();
+    });
+    $("#leftPanel").click(function(event) {
+        event.stopPropagation();
+    });
+    $("#keyboard").click(function(event) {
+        event.stopPropagation();
+        try {
+            currentFocusedInput.addClass('focused');
+        } catch (e) {}
+    });
+    var productTile = function(event) {
+        //console.log($(this).data("objecttype") + "/" + $(this).data("objectid"));
+        if($(this).data("objecttype") == "subcategory" && $(this).data("objectid") != "returnBack"){
+            productPath += "/" + $(this).data("objectid");
+        } else if($(this).data("objectid") == "returnBack"){
+            productPath = "";
+        }
+        loadData(config.dataLocation);
+        $(document).trigger('tileReload');
+    };
+    $(document).on('tileReload', function(event) {
+        $(".productTile").click(productTile);
+    });
+    $(".productTile").click(productTile);
 });
-function addItemToWindow(data){
 
-var subcategoryTemplate ='<div class="productTile" data-objectid="' + data.subcategoryId + '" data-objecttype="subcategory"><img src="' + data.picture + '"/><div class="subcategory"></div><div class="name">' + data.name + '</div></div>';
-var productTemplate ='<div class="productTile" data-objectid="' + data.productId + '" data-objecttype="product"><img src="' + data.picture + '"/><div class="price">' + data.price + '</div><div class="name">' + data.name + '</div></div>';
+function addItemToWindow(data) {
+    if (data.itemType == "product") {
+        var productTemplate = '<div class="productTile" data-objectid="' + data.itemId + '" data-objecttype="product"><img src="' + data.picture + '"/><div class="price">' + money.format.numberToPrice(data.price) + '</div><div class="name">' + data.name + '</div></div>';
+    	$("#productsContainer").append(productTemplate)
+    } else if (data.itemType == "subcategory") {
+        var subcategoryTemplate = '<div class="productTile" data-objectid="' + data.itemId + '" data-objecttype="subcategory"><img src="' + data.picture + '"/><div class="subcategory"></div><div class="name">' + data.name + '</div></div>';
+    	$("#productsContainer").append(subcategoryTemplate)
+    } else{
+    	console.error("unknown item type", data);
+    }
 }
--- app/js/work.js
+++ app/js/work.js
@@ -1,9 +1,26 @@
-var fs = require("fs");
 var database = "NLY";
+var productPath = "";
 
 function loadData(dataLocation) {
     if (dataLocation = "local") {
         database = require("./database.json");
     }
-    
+    $("#productsContainer").html("");
+    if (productPath.split("/")[productPath.split("/").length - 1] == "") {
+        for (var i = 0; i < database.subcategories.length; i++) {
+            addItemToWindow(database.subcategories[i]);
+        }
+    } else {
+        addItemToWindow({
+            itemType: "subcategory",
+            itemId: "returnBack",
+            picture: "./assets/images/Miniatures/back.png",
+            name: "Retour"
+        });
+    }
+    for (var i = 0; i < database.products.length; i++) {
+        if (database.products[i].subCat == productPath.split("/")[productPath.split("/").length - 1]) {
+            addItemToWindow(database.products[i]);
+        }
+    }
 }
