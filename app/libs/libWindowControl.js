0a8c0925fa08340e22f2034f4304d45191029632
StrowBeary <remicaillot5@gmail.com>
2017-02-12 13:57:50 +0100

    always start maximized

--- app/libs/libWindowControl.js
+++ app/libs/libWindowControl.js
@@ -1,31 +1,21 @@
 var manifest = require("./package.json");
-if (!manifest.devtools) {
-    if (localStorage.isMaximized == true) {
-        win.maximize();
-        jQuery(".maximize").css('background-image', 'url(assets/images/maximize.svg)');
-    } else if (localStorage.isMaximized == "false") {
-        win.unmaximize();
-        jQuery(".maximize").css('background-image', 'url(assets/images/unmaximize.svg)');
-    } else {
-        localStorage.isMaximized = false
-        win.unmaximize();
-    }
-}
+
+
 (function($) {
     $(document).ready(function() {
         if (!manifest.devtools) {
             $(".devTools").remove();
         }
-        if (localStorage.isMaximized == undefined) {
-            localStorage.isMaximized = false;
-            win.unmaximize()
+        try{
+            win.maximize();
+            jQuery(".maximize").css('background-image', 'url(assets/images/maximize.svg)');
+        } catch(e){
+            console.log(e);
         }
         win.on('maximize', function() {
-            localStorage.isMaximized = true;
             $(".maximize").css('background-image', 'url(assets/images/unmaximize.svg)');
         });
         win.on('unmaximize', function() {
-            localStorage.isMaximized = false;
             $(".maximize").css('background-image', 'url(assets/images/maximize.svg)');
         });
         $(".maximize").click(function() {
