1456530d21cf0e0eff7b8215603a6d63fba27181
StrowBeary <remicaillot5@gmail.com>
2017-02-18 15:50:24 +0100

    totaux par catégories

--- app/libs/libposPrint.js
+++ app/libs/libposPrint.js
@@ -162,6 +162,7 @@ function printLogs(log) {
                 printer.leftRight("5,5", money.format.numberToPrice(val["5,5"], true));
                 printer.leftRight("10", money.format.numberToPrice(val["10"], true));
                 printer.leftRight("20", money.format.numberToPrice(val["20"], true));
+                printer.leftRight("TOTAL", money.format.numberToPrice(val["5,5"] + val["10"] + val["20"], true))
             });
             printer.newLine();
             printer.drawLine()
