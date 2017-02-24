c27df78dee32a46ec86fac6634f06379c92b2629
StrowBeary <remicaillot5@gmail.com>
2017-02-24 21:02:10 +0100

    libVault

--- app/js/work.js
+++ app/js/work.js
@@ -1,16 +1,38 @@
 var database = "NLY";
 var currentCategory = "";
 var productPath = "";
+
 var Datastore = require('nedb'),
-    commandDb = new Datastore({ filename: './commands.lead', autoload: true }),
-    cashDrawerDb = new Datastore({ filename: './cashDrawer.lead', autoload: true });
+    commandDb,
+    cashDrawerDb,
+    vault = new Vault();
+
+$(document).ready(function () {
+    commandDb = new Datastore({filename: './commands.lead'});
+    cashDrawerDb = new Datastore({filename: './cashDrawer.lead'});
+
+    vault.on("firstRun", function () {
+        vault.lockFile('./commands.lead');
+    });
+
+    vault.setSecret(sessionStorage.getItem("secret"), function(){
+        vault.checkLock('./commands.lead', function (unmodified) {
+            //if lock is verified
+            if (unmodified) {
+                console.log("file modification locked");
+            }
+        });
+    });
+
+});
+
 function loadData(firstLoad) {
 
-        database = JSON.parse(fs.readFileSync("./database.json", "UTF-8"));
+    database = JSON.parse(fs.readFileSync("./database.json", "UTF-8"));
 
     $("#productsContainer").html("");
-    if(firstLoad){
-    	for (var i = 0; i < database.categories.length; i++) {
+    if (firstLoad) {
+        for (var i = 0; i < database.categories.length; i++) {
             addItemToMenu(database.categories[i]);
             if (database.categories[i].defaultFocused) {
                 currentCategory = database.categories[i].itemId;
@@ -18,7 +40,7 @@ function loadData(firstLoad) {
         }
     }
     if (manifest.devtools) {
-       // console.log(productPath);
+        // console.log(productPath);
     }
     if (productPath.split("/")[productPath.split("/").length - 1] != "") {
         addItemToHome({
@@ -35,12 +57,12 @@ function loadData(firstLoad) {
     }
     for (var i = 0; i < database.products.length; i++) {
         if ((database.products[i].subCat == productPath.split("/")[productPath.split("/").length - 1]) && (database.products[i].category == currentCategory)) {
-            if(database.products[i].price == "free"){
+            if (database.products[i].price == "free") {
 
                 database.products[i].price = "Prix libre";
                 addItemToHome(database.products[i]);
                 database.products[i].price = "free";
-            }else{
+            } else {
 
                 database.products[i].price = money.format.numberToPrice(database.products[i].price);
                 addItemToHome(database.products[i]);
@@ -50,39 +72,39 @@ function loadData(firstLoad) {
     }
 
 }
-function search(keyword){
+function search(keyword) {
     var searchResults = {
         products: [],
         categories: [],
         subcategories: []
     };
-    try{
+    try {
         var regex = new RegExp("(" + keyword + "+)[a-z]*", "ig");
-    }catch (err){
+    } catch (err) {
         return {};
     }
-    for(var i = 0; i < database.products.length; i++){
-        if(database.products[i].name.match(regex)){
+    for (var i = 0; i < database.products.length; i++) {
+        if (database.products[i].name.match(regex)) {
             searchResults.products.push(database.products[i]);
         }
     }
-    for(var i = 0; i < database.categories.length; i++){
-        if(database.categories[i].name.match(regex)){
+    for (var i = 0; i < database.categories.length; i++) {
+        if (database.categories[i].name.match(regex)) {
             searchResults.categories.push(database.categories[i]);
         }
     }
-    for(var i = 0; i < database.subcategories.length; i++){
-        if(database.subcategories[i].name.match(regex)){
+    for (var i = 0; i < database.subcategories.length; i++) {
+        if (database.subcategories[i].name.match(regex)) {
             searchResults.subcategories.push(database.subcategories[i]);
         }
     }
-    if(searchResults.products.length == 0){
+    if (searchResults.products.length == 0) {
         delete searchResults.products;
     }
-    if(searchResults.categories.length == 0){
+    if (searchResults.categories.length == 0) {
         delete searchResults.categories;
     }
-    if(searchResults.subcategories.length == 0){
+    if (searchResults.subcategories.length == 0) {
         delete searchResults.subcategories;
     }
     return searchResults;
